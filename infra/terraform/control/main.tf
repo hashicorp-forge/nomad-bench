5107b6831c5353db2f3e2faa9319007fc9626923
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2024-01-29 18:29:20 +0100

    benchmarking: ALB and revamped networking (#425)

--- infra/terraform/control/main.tf
+++ infra/terraform/control/main.tf
@@ -34,11 +34,11 @@ resource "aws_instance" "nomad_server" {
   count                       = var.server_count
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.nomad_server_instance_type
-  subnet_id                   = aws_subnet.nomad_subnet.id
+  subnet_id                   = "${element(local.private_subnet_ids, count.index)}"
   vpc_security_group_ids      = [aws_security_group.nomad_sg.id]
   key_name                    = module.keys.key_name
   iam_instance_profile        = aws_iam_instance_profile.nomad_instance_profile.id
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
   user_data = templatefile("${path.module}/nomad.sh", {
     nomad_conf = templatefile("${path.module}/nomad_server.hcl", {
@@ -54,6 +54,10 @@ resource "aws_instance" "nomad_server" {
     iops        = var.nomad_server_iops
   }
 
+  metadata_options {
+    http_tokens = "required"
+  }
+
   tags = {
     Name       = "${var.cluster_name}_server_${count.index}"
     Nomad_role = "${var.cluster_name}_server"
@@ -64,11 +68,11 @@ resource "aws_instance" "nomad_client" {
   count                       = var.client_count
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.nomad_client_instance_type
-  subnet_id                   = aws_subnet.nomad_subnet.id
+  subnet_id                   = "${element(local.private_subnet_ids, count.index)}"
   vpc_security_group_ids      = [aws_security_group.nomad_sg.id]
   key_name                    = module.keys.key_name
   iam_instance_profile        = aws_iam_instance_profile.nomad_instance_profile.id
-  associate_public_ip_address = true
+  associate_public_ip_address = false
 
   user_data = templatefile("${path.module}/nomad.sh", {
     nomad_conf = templatefile("${path.module}/nomad_client.hcl", {
@@ -84,27 +88,122 @@ resource "aws_instance" "nomad_client" {
     iops        = var.nomad_client_iops
   }
 
+  metadata_options {
+    http_tokens = "required"
+  }
+
   tags = {
     Name       = "${var.cluster_name}_client_${count.index}"
     Nomad_role = "${var.cluster_name}_client"
   }
 }
 
+resource "aws_instance" "bastion" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = "${element(local.public_subnet_ids, 0)}"
+  vpc_security_group_ids      = [aws_security_group.nomad_sg.id]
+  key_name                    = module.keys.key_name
+  iam_instance_profile        = aws_iam_instance_profile.nomad_instance_profile.id
+  associate_public_ip_address = true
+
+  provisioner "file" {
+    source      = module.keys.private_key_filepath
+    destination = "/home/ubuntu/nomad-hosts-key.pem"
+  }
+
+  provisioner "file" {
+    content = <<EOT
+ssh into servers with:
+ %{for ip in aws_instance.nomad_server.*.private_ip~}
+   ssh -i nomad-hosts-key.pem ubuntu@${ip}
+ %{endfor~}
+ssh into clients with:
+ %{for ip in aws_instance.nomad_client.*.private_ip~}
+    ssh -i nomad-hosts-key.pem ubuntu@${ip}
+ %{endfor~}
+EOT
+
+    destination = "/home/ubuntu/nomad-hosts.txt"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file(module.keys.private_key_filepath)
+    host        = self.public_ip
+  }
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  tags = {
+    Name = "${var.cluster_name}_bastion_host"
+  }
+}
+
+resource "aws_lb" "nomad_lb" {
+  name               = "${var.cluster_name}-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.nomad_lb.id]
+  subnets            = local.public_subnet_ids
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "${var.cluster_name}_lb"
+  }
+}
+
+resource "aws_lb_listener" "nomad_listener" {
+  load_balancer_arn = aws_lb.nomad_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nomad_tg.arn
+  }
+
+  tags = {
+    Name = "${var.cluster_name}_listener"
+  }
+}
+
+resource "aws_lb_target_group" "nomad_tg" {
+  name     = "${var.cluster_name}-nomad-tg"
+  port     = 4646
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.nomad_vpc.id
+  health_check {
+    path = "/v1/jobs"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "nomad" {
+  for_each = {
+    for k, v in aws_instance.nomad_server :
+    v.tags["Name"] => v
+  }
+
+  target_group_arn = aws_lb_target_group.nomad_tg.arn
+  target_id        = each.value.id
+  port             = 4646
+}
+
 output "message" {
   value = <<EOM
 Your cluster has been provisioned!
 
-ssh into servers with:
+Load balancer address: ${aws_lb.nomad_lb.dns_name}
 
-%{for ip in aws_instance.nomad_server.*.public_ip~}
-   ssh -i keys/${local.random_name}.pem ubuntu@${ip}
-%{endfor~}
-
-ssh into clients with:
-
-%{for ip in aws_instance.nomad_client.*.public_ip~}
-    ssh -i keys/${local.random_name}.pem ubuntu@${ip}
-%{endfor~}
-
+SSH into the bastion host:
+ssh -i keys/${local.random_name}.pem ubuntu@${aws_instance.bastion.public_ip}
 EOM
 }
--- infra/terraform/control/networking.tf
+++ infra/terraform/control/networking.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "nomad_test_vpc" {
+resource "aws_vpc" "nomad_vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
@@ -7,64 +7,127 @@ resource "aws_vpc" "nomad_test_vpc" {
   }
 }
 
-resource "aws_internet_gateway" "nomad_test_igw" {
-  vpc_id = aws_vpc.nomad_test_vpc.id
+resource "aws_internet_gateway" "nomad_igw" {
+  vpc_id = aws_vpc.nomad_vpc.id
 
   tags = {
     Name = "${var.cluster_name}-igw"
   }
 }
 
-resource "aws_default_route_table" "nomad_test_route_table" {
-  default_route_table_id = aws_vpc.nomad_test_vpc.default_route_table_id
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.nomad_test_igw.id
-  }
+locals {
+  az_names = data.aws_availability_zones.available.names
+}
 
+resource "aws_subnet" "nomad_lb" {
+  for_each                = { for index, az_name in local.az_names : index => az_name }
+  vpc_id                  = aws_vpc.nomad_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.nomad_vpc.cidr_block, 8, each.key + 1)
+  availability_zone       = local.az_names[each.key]
+  map_public_ip_on_launch = false
   tags = {
-    Name = "${var.cluster_name}-route_table"
+    Name = "${var.cluster_name}-${local.az_names[each.key]}-public-subnet"
   }
 }
 
-resource "aws_subnet" "nomad_subnet" {
-  vpc_id     = aws_vpc.nomad_test_vpc.id
-  cidr_block = "10.0.1.0/24"
+resource "aws_subnet" "nomad_instances" {
+  for_each                = { for index, az_name in local.az_names : index => az_name }
+  vpc_id                  = aws_vpc.nomad_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.nomad_vpc.cidr_block, 8, 3 + each.key + 1)
+  availability_zone       = local.az_names[each.key]
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${var.cluster_name}-${local.az_names[each.key]}-private-subnet"
+  }
+}
+
+locals {
+  public_subnet_ids  = [for s in aws_subnet.nomad_lb : s.id]
+  private_subnet_ids = [for s in aws_subnet.nomad_instances : s.id]
+}
+
+resource "aws_eip" "nomad_natgw_eip" {
+  domain = "vpc"
+  tags = {
+    Name = "${var.cluster_name}_natgw_eip"
+  }
+}
+
+resource "aws_nat_gateway" "nomad_nat_gw" {
+  allocation_id     = aws_eip.nomad_natgw_eip.id
+  subnet_id         = aws_subnet.nomad_lb[0].id
+  connectivity_type = "public"
 
   tags = {
-    Name = "${var.cluster_name}-subnet"
+    Name = "${var.cluster_name}-nat-gw"
   }
+
+  depends_on = [aws_internet_gateway.nomad_igw]
+}
+
+resource "aws_route_table" "nomad_igw_route_table" {
+  vpc_id = aws_vpc.nomad_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.nomad_igw.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-igw-route_table"
+  }
+}
+
+resource "aws_route_table" "nomad_natgw_route_table" {
+  vpc_id = aws_vpc.nomad_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nomad_nat_gw.id
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-natgw-route_table"
+  }
+}
+
+resource "aws_route_table_association" "igw" {
+  count          = 3
+  subnet_id      = "${element(local.public_subnet_ids, count.index)}"
+  route_table_id = aws_route_table.nomad_igw_route_table.id
+}
+
+resource "aws_route_table_association" "natgw" {
+  count          = 3
+  subnet_id      = "${element(local.private_subnet_ids, count.index)}"
+  route_table_id = aws_route_table.nomad_natgw_route_table.id
 }
 
 data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
 
-resource "aws_security_group" "nomad_sg" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.nomad_test_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
-  }
-
-  ingress {
-    from_port   = 4646
-    to_port     = 4646
-    protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
-  }
+resource "aws_security_group" "nomad_lb" {
+  name        = "${var.cluster_name}-lb-sg"
+  description = "Allow inbound traffic to Nomad ALB"
+  vpc_id      = aws_vpc.nomad_vpc.id
 
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["10.0.1.0/24"]
+    cidr_blocks = [aws_vpc.nomad_vpc.cidr_block]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
   }
 
   egress {
@@ -76,6 +139,58 @@ resource "aws_security_group" "nomad_sg" {
   }
 
   tags = {
-    Name = "${var.cluster_name}-sg"
+    Name = "${var.cluster_name}-lb-sg"
+  }
+}
+
+resource "aws_security_group" "nomad_sg" {
+  name   = "${var.cluster_name}-nomad-sg"
+  vpc_id = aws_vpc.nomad_vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.nomad_lb.id]
+  }
+
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    self      = true
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [aws_vpc.nomad_vpc.cidr_block]
+  }
+
+  ingress {
+    from_port   = 4646
+    to_port     = 4646
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.cluster_name}-nomad-sg"
   }
 }
