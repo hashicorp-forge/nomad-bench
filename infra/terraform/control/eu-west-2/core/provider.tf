ba91a670b88a41ee3068adfb30466ce1faff0030
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2024-02-01 16:21:09 +0100

    nomad bench: lock state (#9)

--- infra/terraform/control/eu-west-2/core/provider.tf
+++ infra/terraform/control/eu-west-2/core/provider.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "nomad-bench"
-    key    = "tf-state/eu-west-2/bench-core"
-    region = "eu-west-2"
+    bucket         = "nomad-bench"
+    key            = "tf-state/eu-west-2/bench-core"
+    region         = "eu-west-2"
+    dynamodb_table = "nomad-bench-terraform-state-lock"
   }
 }
 
