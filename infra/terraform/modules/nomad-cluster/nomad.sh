676ae865f9f0441c86d7002cca45aba0000ffddf
James Rasell <jrasell@hashicorp.com>
2024-02-06 09:07:44 +0000

    infra: install Docker via Ansible.

--- infra/terraform/modules/nomad-cluster/nomad.sh
+++ infra/terraform/modules/nomad-cluster/nomad.sh
@@ -2,7 +2,6 @@
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 apt update && apt install -y --no-install-recommends \
-    docker.io \
     nomad \
     podman \
     tzdata
