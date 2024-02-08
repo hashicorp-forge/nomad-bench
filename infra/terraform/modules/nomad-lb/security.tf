b098b85d1e9c20db8368aa54b311e627f7446a4f
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2024-02-08 16:32:38 +0100

    LB SG must allow 22 now for ansible

--- infra/terraform/modules/nomad-lb/security.tf
+++ infra/terraform/modules/nomad-lb/security.tf
@@ -26,6 +26,24 @@ resource "aws_security_group_rule" "user_ingress_80" {
   to_port           = 80
 }
 
+resource "aws_security_group_rule" "user_ingress_443" {
+  security_group_id = aws_security_group.lb.id
+  cidr_blocks       = var.user_ingress_ips
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 443
+  to_port           = 443
+}
+
+resource "aws_security_group_rule" "user_ingress_22" {
+  security_group_id = aws_security_group.lb.id
+  cidr_blocks       = var.user_ingress_ips
+  type              = "ingress"
+  protocol          = "tcp"
+  from_port         = 22
+  to_port           = 22
+}
+
 resource "aws_security_group_rule" "all_egress" {
   security_group_id = aws_security_group.lb.id
   cidr_blocks       = ["0.0.0.0/0"]
