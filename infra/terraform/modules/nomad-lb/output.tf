0d606daa6ba28b00f2c302c27e2ac9c147ca374f
James Rasell <jrasell@hashicorp.com>
2024-02-12 09:29:37 +0000

    infra: additional Nomad LB TF module outputs.

--- infra/terraform/modules/nomad-lb/output.tf
+++ infra/terraform/modules/nomad-lb/output.tf
@@ -1,5 +1,9 @@
-output "lb_ip" {
+output "lb_public_ip" {
   value = aws_instance.nomad_nginx_lb.public_ip
+}
+
+output "lb_private_ip" {
+  value = aws_instance.nomad_nginx_lb.private_ip
 }
 
 output "lb_sg_id" {
