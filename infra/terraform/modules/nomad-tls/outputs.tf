ad172657b66b2237ff7ec64ec0447af6878575eb
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2024-02-13 20:06:15 +0100

    cleanups

--- infra/terraform/modules/nomad-tls/outputs.tf
+++ infra/terraform/modules/nomad-tls/outputs.tf
@@ -9,11 +9,3 @@ output "nomad_client_cert_path" {
 output "nomad_client_key_path" {
   value = "${var.tls_output_path}/global-client-nomad-key.pem"
 }
-
-output "nomad_cli_cert_path" {
-  value = "${var.tls_output_path}/global-cli-nomad.pem"
-}
-
-output "nomad_cli_key_path" {
-  value = "${var.tls_output_path}/global-cli-nomad-key.pem"
-}
