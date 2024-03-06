451cae56d424adab7f05c5d48b4211f5a72c4d43
Luiz Aoqui <luiz@hashicorp.com>
2024-03-05 23:08:20 -0500

    core: use Terraform Cloud as backend
    Since we're storing credentials for long-running infrastructure it may
    be better to store them in a HashiCorp service instead of a random S3
    bucket.
    
    TFC also provides more visibility on changes and can be useful when
    debugging problems with the rest of the team.
    
    It also doesn't require AWS credentials from Doormat to be loaded into
    the local machine.

--- infra/eu-west-2/core/provider.tf
+++ infra/eu-west-2/core/provider.tf
@@ -1,19 +1,35 @@
 terraform {
-  backend "s3" {
-    bucket         = "nomad-bench"
-    key            = "tf-state/eu-west-2/bench-core"
-    region         = "eu-west-2"
-    dynamodb_table = "nomad-bench-terraform-state-lock"
+  cloud {
+    organization = "nomad-eng"
+
+    workspaces {
+      name = "nomad-bench-core"
+    }
   }
 
   required_providers {
     ansible = {
-      version = "~> 1.1.0"
       source  = "ansible/ansible"
+      version = "~> 1.1.0"
+    }
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
     }
   }
 }
 
+provider "doormat" {}
+
 provider "aws" {
   region = var.region
+
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
+}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+  role_arn = "arn:aws:iam::999225027745:role/tfc-doormat-nomad-bench-core"
 }
