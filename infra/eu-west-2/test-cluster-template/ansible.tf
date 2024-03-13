10253afad32d0a332bcd1dcb9ff84ddb0f455694
Luiz Aoqui <luiz@hashicorp.com>
2024-03-13 13:41:55 -0400

    test-cluster: create host and group vars overrides
    Pre-create host_vars and group_vars files so it is easier to customize
    test cluster configuration.
    
    Also remove test clusters from `.gitignore`. In hindsight it can be
    useful to be able to push their code, even if it not intended to be
    merged to `main`.

--- infra/eu-west-2/test-cluster-template/ansible.tf
+++ infra/eu-west-2/test-cluster-template/ansible.tf
@@ -24,7 +24,7 @@ EOT
 
 resource "ansible_group" "server" {
   name     = "server"
-  children = flatten([for k, v in module.clusters : v.ansible_group_server])
+  children = flatten([for k, v in module.clusters : v.server_ansible_group])
   variables = merge(
     local.ansible_default_vars,
     local.ansible_influxdb_telegraf_default_vars,
@@ -47,5 +47,62 @@ resource "ansible_host" "bastion" {
   groups = [ansible_group.bastion.name]
   variables = {
     ansible_host = data.terraform_remote_state.core.outputs.bastion_ip
+  }
+}
+
+# Can't use local_file resource with ignore_changes because the resource ID is
+# its hash, so changes are never ignored.
+# https://github.com/hashicorp/terraform-provider-local/issues/262
+resource "terraform_data" "host_vars" {
+  for_each = toset(flatten([for v, c in module.clusters : c.server_ansible_hosts]))
+
+  provisioner "local-exec" {
+    command = <<EOF
+mkdir -p ${path.module}/ansible/host_vars
+cat <<EOH > ${path.module}/ansible/host_vars/${each.key}.yaml
+# Variable overrides for host ${each.key}.
+#
+# This file is created by Terraform, but not managed, so it is not recreated if
+# deleted.
+#
+# Replace the resource to recreate this file.
+# ALL CHANGES WILL BE LOST.
+#   terraform apply -replace 'terraform_data.host_vars["${each.key}"]'
+EOH
+EOF
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+rm -f ${path.module}/ansible/host_vars/${each.key}.yaml
+EOF
+    when    = destroy
+  }
+}
+
+resource "terraform_data" "group_vars" {
+  for_each = toset([for v, c in module.clusters : c.server_ansible_group])
+
+  provisioner "local-exec" {
+    command = <<EOF
+mkdir -p ${path.module}/ansible/group_vars
+cat <<EOH > ${path.module}/ansible/group_vars/${each.key}.yaml
+# Variable overrides for group ${each.key}.
+#
+# This file is created by Terraform, but not managed, so it is not recreated if
+# deleted.
+#
+# Replace the resource to recreate this file.
+# ALL CHANGES WILL BE LOST.
+#   terraform apply -replace 'terraform_data.group_vars["${each.key}"]'
+EOH
+EOF
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+rm -f ${path.module}/ansible/group_vars/${each.key}.yaml
+EOF
+    when    = destroy
   }
 }
