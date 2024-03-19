e78a28923cd503e1850f028618e28def9017f1e5
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2024-03-19 16:08:57 +0100

    version and makefile

---
+++ tools/nomad-load/version/version.go
@@ -0,0 +1,9 @@
+package version
+
+var (
+	// VERSION indicates which version of the binary is running.
+	VERSION string
+
+	// GITCOMMIT indicates which git hash the binary was built off of
+	GITCOMMIT string
+)
