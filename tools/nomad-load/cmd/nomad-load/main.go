09568b8cc9346bdc96698ec6f70a39653463a4bc
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2024-03-19 16:09:19 +0100

    cmd

--- tools/nomad-load/cmd/nomad-load/main.go
+++ tools/nomad-load/cmd/nomad-load/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"context"
-	_ "embed"
 	"flag"
 	"fmt"
 	"net/http"
 	"os"
 	"runtime"
-	"strings"
 	"time"
 
 	"github.com/hashicorp/go-hclog"
@@ -16,17 +14,13 @@ import (
 	metricsprom "github.com/hashicorp/go-metrics/prometheus"
 	"github.com/hashicorp/nomad/api"
 	"github.com/hashicorp/nomad/jobspec2"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"golang.org/x/sync/errgroup"
 	"golang.org/x/time/rate"
 
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/hashicorp/nomad-bench/tools/nomad-load/version"
 )
-
-const version = "0.0.1"
-
-//go:embed assets/job.nomad.hcl
-var defaultJob string
 
 var (
 	nomadAddr = flag.String("nomad-addr", "", "The address of the Nomad server")
@@ -34,17 +28,45 @@ var (
 	httpPort  = flag.String("http-port", "8080", "The port to bind the HTTP server to")
 	reqRate   = flag.Float64("rate", 10, "The rate of job dispatches per second")
 	workers   = flag.Int("workers", 10*runtime.NumCPU(), "The number of workers to use")
+	duration  = flag.Duration("duration", time.Minute, "The duration to run the test for")
 	logLevel  = flag.String("log-level", "DEBUG", "The log level to use")
 )
 
 func main() {
 	flag.Parse()
 
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: ./nomad-load [options] pathToJobfile
+
+Options:
+`)
+		flag.PrintDefaults()
+	}
+
 	logger := hclog.NewInterceptLogger(&hclog.LoggerOptions{
 		Name:            "nomad-load",
 		Level:           hclog.LevelFromString(*logLevel),
 		IncludeLocation: true,
 	})
+
+	arg := flag.Args()
+	if len(arg) != 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if arg[0] == "version" {
+		fmt.Printf("Version: %s\nCommit: %s\n", version.VERSION, version.GITCOMMIT)
+		os.Exit(0)
+	}
+
+	jobFilePath := arg[0]
+
+	jobFile, err := os.Open(jobFilePath)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
 
 	// Start metrics collection.
 	promHandler := promhttp.HandlerFor(prometheus.DefaultGatherer, promhttp.HandlerOpts{
@@ -60,7 +82,9 @@ func main() {
 	metrics.NewGlobal(metrics.DefaultConfig("nomad-load"), promSink)
 
 	// Create errgroup to watch goroutines.
-	g, ctx := errgroup.WithContext(context.Background())
+	ctx, cancel := context.WithTimeout(context.Background(), *duration)
+	g, ctx := errgroup.WithContext(ctx)
+	defer cancel()
 
 	// Start HTTP server for metrics.
 	mux := http.NewServeMux()
@@ -81,8 +105,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	r := strings.NewReader(defaultJob)
-	j, err := jobspec2.Parse("job.nomad.hcl", r)
+	j, err := jobspec2.Parse("job.nomad.hcl", jobFile)
 	if err != nil {
 		logger.Error("failed to parse job", "error", err)
 		os.Exit(1)
@@ -95,6 +118,7 @@ func main() {
 	}
 
 	// Start goroutines to dispatch job.
+	logger.Info("dispatching %v jobs at a rate of %v per second for %v", *workers, *reqRate, *duration)
 	l := rate.NewLimiter(rate.Limit(*reqRate), 1)
 	for i := 0; i < *workers; i++ {
 		g.Go(func() error {
