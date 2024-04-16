3e8c05d4f5f35aa87871b81c830cd91bdd0cbb9f
James Rasell <jrasell@hashicorp.com>
2024-04-16 08:30:45 +0100

    infra: remove old style test cluster example dir

--- infra/eu-west-2/jrasell-b20b731/output.tf
+++
@@ -1,3 +0,0 @@
-output "message" {
-  value = module.output.message
-}
--- infra/eu-west-2/jrasell-b20b731/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "region" {}
-variable "project_name" {}
-
-variable "ssh_key_path" {}
-variable "bastion_ip" {}
-variable "private_subnet_ids" {}
-variable "nomad_security_group_id" {}
-variable "nomad_lb_private_ip_address" {}
