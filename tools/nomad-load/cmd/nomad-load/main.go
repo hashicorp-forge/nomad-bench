af48d8778287f48c7a58ac2c77e322be04153112
Piotr Kazmierczak <470696+pkazmierczak@users.noreply.github.com>
2024-05-13 14:39:07 +0200

    nomad-load: support increasing updates frequency

--- tools/nomad-load/cmd/nomad-load/main.go
+++ tools/nomad-load/cmd/nomad-load/main.go
@@ -27,20 +27,21 @@ var (
 	httpAddr  = flag.String("http-addr", "0.0.0.0", "The address to bind the HTTP server to")
 	httpPort  = flag.String("http-port", "8080", "The port to bind the HTTP server to")
 
-	jobType         = flag.String("type", internal.JobTypeBatch, "The type of job to create (batch or service)")
-	jobDriver       = flag.String("driver", internal.JobDriverMock, "The driver to use for the job (mock or docker)")
-	updatesDelay    = flag.Duration("updates-delay", 500*time.Millisecond, "The delay between updates of the service job")
-	numOfUpdates    = flag.Int("num-of-updates", 0, "The number of updates to perform on the service job. If 0, the job will be continuosly updated")
-	numOfDispatches = flag.Int("num-of-dispatches", 0, "The number of dispatches to perform on the batch job. If 0, the job will be continuosly dispatched")
-	count           = flag.Int("count", 1, "The count number per job (number of allocations is count * groups)")
-	groups          = flag.Int("groups", 1, "The number of groups to create per job")
-	spread          = flag.Bool("spread", false, "Should the jobs be spread across the datacenters?")
-	reqRate         = flag.Float64("rate", 10, "The rate of constant job dispatches per second")
-	burstRate       = flag.Int("burst", 1, "The burst rate of constant job dispatches")
-	randomize       = flag.Bool("random", false, "Should the rate at which the jobs are dispatched be randomized?")
-	seed1           = flag.Uint64("seed1", rand.Uint64(), "First uint64 of the PCG seed used by the random number generator")
-	seed2           = flag.Uint64("seed2", rand.Uint64(), "Second uint64 of the PCG seed used by the random number generator")
-	workers         = flag.Int("workers", 10*runtime.NumCPU(), "The number of workers to use")
+	jobType            = flag.String("type", internal.JobTypeBatch, "The type of job to create (batch or service)")
+	jobDriver          = flag.String("driver", internal.JobDriverMock, "The driver to use for the job (mock or docker)")
+	updatesDelay       = flag.Duration("updates-delay", 3*time.Second, "The delay between updates of the service job")
+	updatesDelayTarget = flag.Duration("updates-delay-target", 0, "The target delay between updates. For finite num-of-updates values, this will be delay reached before the last update. Ignored if unset")
+	numOfUpdates       = flag.Int("num-of-updates", 0, "The number of updates to perform on the service job. If 0, the job will be continuosly updated")
+	numOfDispatches    = flag.Int("num-of-dispatches", 0, "The number of dispatches to perform on the batch job. If 0, the job will be continuosly dispatched")
+	count              = flag.Int("count", 1, "The count number per job (number of allocations is count * groups)")
+	groups             = flag.Int("groups", 1, "The number of groups to create per job")
+	spread             = flag.Bool("spread", false, "Should the jobs be spread across the datacenters?")
+	reqRate            = flag.Float64("rate", 10, "The rate of constant job dispatches per second")
+	burstRate          = flag.Int("burst", 1, "The burst rate of constant job dispatches")
+	randomize          = flag.Bool("random", false, "Should the rate at which the jobs are dispatched be randomized?")
+	seed1              = flag.Uint64("seed1", rand.Uint64(), "First uint64 of the PCG seed used by the random number generator")
+	seed2              = flag.Uint64("seed2", rand.Uint64(), "Second uint64 of the PCG seed used by the random number generator")
+	workers            = flag.Int("workers", 10*runtime.NumCPU(), "The number of workers to use")
 
 	logLevel = flag.String("log-level", "DEBUG", "The log level to use")
 	ver      = flag.Bool("version", false, "Prints out the version")
@@ -118,7 +119,7 @@ func main() {
 		case internal.JobTypeBatch:
 			go job.DispatchBatch(&wg, *numOfDispatches, lim, rng)
 		case internal.JobTypeService:
-			go job.RunService(&wg, i, *numOfUpdates, *updatesDelay)
+			go job.RunService(&wg, i, *numOfUpdates, *updatesDelay, *updatesDelayTarget)
 		}
 	}
 
